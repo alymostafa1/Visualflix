39889811407dc6cd34eb4ff3bb2d865e30db5d44
Yara Zaki <61282472+yarazakii@users.noreply.github.com>
2021-05-25 16:41:08 +0200

    Updated HandDetection

--- src/HandDetection/Api.py
+++ src/HandDetection/Api.py
@@ -5,7 +5,25 @@ import pyautogui as p
 import math
 import time
 import copy
-    
+
+
+
+flag = 0
+Zero_flag = 1000
+time_flag = 0
+time_flag_2 = 0
+count_defects = 0
+fake_cont = []
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+        return t 
+
     
 def RGB_Threshold(ROI):
     bgModel = cv2.createBackgroundSubtractorMOG2(0, 50)
@@ -37,21 +55,18 @@ def YCrCb(ROI):
 def ContourLocator(CurrentFrame, ROI):
     
         contours, hierarchy = cv2.findContours(CurrentFrame,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
-                       
+        # print(len(contours))
+        # fake_cont = [181 412]
+        
+        # if(len(contours) == 0):
+        #     print("NoneType contour")
+        #     contours.append([[181 , 412]])
+        # print("----------------------------")
+        # print(len(contours))
+        
         # find contour with max area    
         contour = max(contours, key = lambda x: cv2.contourArea(x))
-        
-        # create bounding rectangle around the contour (can skip below two lines)
-        x, y, w, h = cv2.boundingRect(contour)
-        conto = cv2.rectangle(ROI, (x, y), (x+w, y+h), (0, 0, 255), 2)
-    
-        # finding convex hull
-        hull = cv2.convexHull(contour)
-    
-        # # drawing contours
-        # drawing = np.zeros(ROI.shape,np.uint8)
-        # # cv2.drawContours(drawing, [contour], 0, (0, 255, 0), 0)
-    
+                
         # finding convex hull
         hull = cv2.convexHull(contour, returnPoints=False)
     
@@ -84,14 +99,50 @@ def DetectAngle(defects, display, contour):
                 cv2.circle(display, start , 1, [255,0,0], 5)
     
             # ignore angles > 90 and highlight rest with red dots
-            if angle <= 90 :
+            if angle <= 90:
                 count_defects += 1
                 cv2.circle(display, far, 1, [100,0,255], 5)
                 if far[1]  > 400: 
                       count_defects -= 1
                       cv2.circle(display, far, 1, [0,0,0], 5)
-                # print(angle)
-            
-                      
+                
         return count_defects
-    
+
+
+def ActionDetector(count_defects ,display ):
+    if count_defects == 0:
+        cv2.putText(display, " ", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (0, 255, 255),
+                    2,
+                    cv2.LINE_4)
+
+        Zero_flag = 0
+    elif count_defects == 1 :
+        p.press("up")
+        cv2.putText(display, "Increase volume" + str(count_defects), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (0, 255, 255),
+                    2,
+                    cv2.LINE_4)
+        flag = 0
+
+    elif count_defects == 2:
+        p.press("down")
+        cv2.putText(display, "Decrease Volume" + str(count_defects), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (0, 255, 255),
+                    2,
+                    cv2.LINE_4)
+        flag = 0
+    elif count_defects == 3:
+        cv2.putText(display, " ", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (0, 255, 255),
+                    2,
+                    cv2.LINE_4)
+        flag = 0
+        
+    elif count_defects == 4 and flag == 0:
+        p.press("space")
+        cv2.putText(display, "start/pause" + str(count_defects), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (0, 255, 255),
+                    2,
+                    cv2.LINE_4)
+        flag = 1
--- src/HandDetection/Hand-dect.py
+++ src/HandDetection/Hand-dect.py
@@ -1,29 +1,21 @@
 from Api import * 
 
 
-
-
-
-
 vid = cv2.VideoCapture(0)
+
 vid.set(3, 800)  # width=800
 vid.set(4, 720)  # height=720 
-if not vid.isOpened():
+if not vid .isOpened():
   print ("Could not open Camera")
   exit()
 
-flag = 0
-time_flag = 0
-time_flag_2 = 0
-count_defects = 0
+
   
 while(True):
-
     ret, frame = vid.read()  
-    display = cv2.rectangle(frame.copy(),(1,1),(300,720),(0,0,0),5) 
-    
-    cv2.imshow('curFrame',display)
-    
+    display = cv2.rectangle(frame.copy(),(1,1),(300,720),(0,0,0),5)    
+    cv2.imshow('curFrame',display.copy())
+      
     ROI = frame[0:500, 0:300].copy() # Region of interest 
     ROI_2 = frame.copy()
     # cv2.imshow('Current Roi', ROI)
@@ -42,32 +34,32 @@ while(True):
     # thresholdin: Otsu's Binarization method
     _, thresh1 = cv2.threshold(blurred, 127, 255,
                                cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)    
-    # cv2.imshow('Thresholded' ,thresh1)
+    cv2.imshow('Thresholded' ,thresh1)
     
     '''
     Second method to threshold the hand 
     '''
     SkinMask = RGB_Threshold(ROI)
-    # cv2.imshow('Skin RGB' ,SkinMask)
+    cv2.imshow('Skin RGB' ,SkinMask)
     
     '''
     Third Method to threshold the hand 
     '''
     GThreshold = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)          
-    # cv2.imshow('Gaussian Threshol ', GThreshold)
+    cv2.imshow('Gaussian Threshol ', GThreshold)
     
     '''
     Fourth Method to threshold the hand 
     '''
     YCrCb_th = YCrCb(ROI)   
-    # cv2.imshow('YCRCB Thresholding', YCrCb_th)
+    cv2.imshow('YCRCB Thresholding', YCrCb_th)
 
-    #'''
     try:  
         '''
         Locating the contours in the ROI after thresholding
-        '''              
-        defects , contours, contour = ContourLocator(SkinMask, ROI)                
+         '''              
+        defects , contours, contour = ContourLocator(YCrCb_th, ROI)
+        # if(len(contours)):
         cv2.drawContours(display, contours, -1, (0, 255, 0), 3)    
         count_defects = 0
         
@@ -75,61 +67,48 @@ while(True):
         Detecting the angle of each Convex Defect and returning the number of Defects
         '''
         count_defects = DetectAngle(defects, display, contour)
- 
-        timer_2 = int(time.time()*1000.0)
-
-        print(timer_2 , time_flag_2)
 
         # define actions required
-        if count_defects == 0 :
-           cv2.putText(display," ", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 
-                    (0, 255, 255), 
-                    2, 
-                    cv2.LINE_4)
-           time_flag = datetime.now().time()          
-           time_flag_2 = int(time.time()*1000.0)         
-        elif count_defects == 1:
+        # ActionDetector(count_defects, display)       
+        if count_defects == 0:
+              cv2.putText(display, " ", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                         (0, 255, 255),
+                         2,
+                         cv2.LINE_4)
+              Zero_flag = 0
+        elif count_defects == 1 :
             p.press("up")
-            cv2.putText(display,"Increase volume" + str(count_defects), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 
-                    (0, 255, 255), 
-                    2, 
-                    cv2.LINE_4)
+            cv2.putText(display, "Increase volume" + str(count_defects), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (0, 255, 255),
+                        2,
+                        cv2.LINE_4)
             flag = 0
-            time_flag_2 = int(time.time()*1000.0)
-            time_flag = datetime.now().time()
-        elif count_defects == 2 :
+        elif count_defects == 2:
             p.press("down")
-            cv2.putText(display,"Decrease Volume" + str(count_defects), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 
-                    (0, 255, 255), 
-                    2, 
-                    cv2.LINE_4)
+            cv2.putText(display, "Decrease Volume" + str(count_defects), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (0, 255, 255),
+                        2,
+                        cv2.LINE_4)
             flag = 0
-            time_flag = datetime.now().time()
-            time_flag_2 = int(time.time()*1000.0)
-        elif count_defects == 3 :
-            cv2.putText(display," ", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 
-                    (0, 255, 255), 
-                    2, 
-                    cv2.LINE_4)
+        elif count_defects == 3:
+            cv2.putText(display, " ", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (0, 255, 255),
+                        2,
+                        cv2.LINE_4)
             flag = 0
-            time_flag = datetime.now().time()
-            time_flag_2 = int(time.time()*1000.0)
-        elif count_defects == 4 and flag == 0 : 
+        elif count_defects == 4 and flag == 0:
             p.press("space")
-            cv2.putText(display,"start/pause" + str(count_defects), (50, 50),cv2.FONT_HERSHEY_SIMPLEX, 1, 
-                    (0, 255, 255), 
-                    2, 
-                    cv2.LINE_4)
+            cv2.putText(display, "start/pause" + str(count_defects), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (0, 255, 255),
+                        2,
+                        cv2.LINE_4)
             flag = 1
-            time_flag = datetime.now().time()
-            time_flag_2 = int(time.time()*1000.0)
-    except:
+    except: 
         continue
     
-    cv2.imshow('curFrame',display)     
-    # print(count_defects , flag )
+    cv2.imshow('curFrame',display.copy())    
+
     if cv2.waitKey(1) & 0xFF == ord('s'): # "S" to quit 
         break
 vid.release()
 cv2.destroyAllWindows()
-        
